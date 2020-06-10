49429873268c97b535755be7591220b2c0e70e37
IrinaLysenko21 <irina.lysenko21@gmail.com>
2020-06-10 17:15:15 +0300

    mobile layout fixed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import './App.css';
 import feed from './feeds/feed_10_06.json';
 
-console.log(feed.rss[0].channel[0].item);
 const products = feed.rss[0].channel[0].item.sort((a, b) => a["g:title"] - b["g:title"]);
 
 function App() {
   return (
     <div className="App">
       <ul className="items_list">
-        {products.map(product => (<li className="item" key={product["g.id"]}>
+        {products.map(product => (<li className="item" key={product["g:id"]}>
           <p className="item_title">{product["g:title"]}</p>
           <div className="content_wrapper">
           <img className="item_img" src={product["g:image_link"]} alt={product["g:id"].toString()}></img>
